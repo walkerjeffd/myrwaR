c636359b625d11f5d0f5c320eef8f1ea788b189e
Jeff Walker <walker.jeff.d@gmail.com>
2015-05-27 15:24:48 -0400

    update precipitation vignette and event function

--- R/events.R
+++ R/events.R
@@ -1,82 +1,93 @@
-assign.events <- function(df, datetime.name="DATETIME", value.name="VALUE",
-                          interevent=8, threshold=0.1) {
-  # assigns events to data frame such as storm events or discharge events
-  require(plyr)
-  if (!(datetime.name %in% names(df))) {
+#' Delineates storm events based on hourly precipitation
+#'
+#' Uses a timeseries of hourly precipitation to define storm events and create
+#' summary table
+#'
+#' @param x Dataframe containing hourly precipitation timeseries
+#' @param datetime.name Name of datetime column
+#' @param value.name Name of precipitation value column
+#' @param interevent.period Minimum inter-event period to distinguish separate
+#'   events
+#' @param threshold.total Minimum total precipitation for an individual event
+#' @export
+#' @return dataframe or zoo object of hourly precipitation values
+assign_precip_events <- function(x, datetime.name="Datetime", value.name="Precip",
+                                 interevent.period=8, threshold.total=0.1) {
+
+  if (!(datetime.name %in% names(x))) {
     stop(paste0("Could not find datetime column called ", datetime.name))
   }
-  if (!(value.name %in% names(df))) {
+
+  if (!(value.name %in% names(x))) {
     stop(paste0("Could not find value column called ", value.name))
   }
-  if (!is.regular(df[, datetime.name])) {
-    stop("Dataframe is not regular")
+
+  if (!check_hourly(x[, datetime.name])) {
+    stop("Timeseries is not hourly regular")
   }
+
+  df <- x
+
+  # extract datetime and value columns
+  x <- dplyr::select_(x, "Datetime"=datetime.name, "Value"=value.name)
+
   # make sure df is sorted by datetime
-  df <- df[order(df[, datetime.name]), ]
+  x <- x[order(x[, "Datetime"]), ]
 
-  # extract datetime and value
-  x <- data.frame(DATETIME=df[, datetime.name],
-                  VALUE=df[, value.name])
+  # compute runs of non-zero values
+  x_rle <- rle(x[, "Value"] > 0)
 
-  # compute runs
-  x_rle <- rle(x$VALUE > 0)
-
-  # EVENT: logical column indicating if timestep is part of event
-  # does not yet account for events that do not meet minimum threshold
-  x$EVENT <- rep(x_rle$lengths * (x_rle$values | x_rle$lengths < interevent),
+  # assign boolean flag to indicate wet events
+  x$IsWet <- rep(x_rle$lengths * (x_rle$values | x_rle$lengths < interevent.period),
                  x_rle$lengths)
-  x$EVENT <- x$EVENT > 0
+  x$IsWet <- x$IsWet > 0
 
-  # EVENT_ID: numeric column assigning unique IDs to each event
-  x$EVENT_ID <- c(as.numeric(x$EVENT[1]),
-                  ifelse(diff(x$EVENT) > 0, diff(x$EVENT), 0))
-  x$EVENT_ID <- cumsum(x$EVENT_ID) * x$EVENT
+  # assign wet event IDs
+#   x$WetEventID <- c(as.numeric(x$IsWet[1]),
+#                     ifelse(diff(x$IsWet) > 0, diff(x$IsWet), 0))
+  x$EventID <- c(as.numeric(x$IsWet[1]), diff(x$IsWet))
+  x$EventID <- abs(x$EventID)
+  x$EventID <- cumsum(x$EventID)
 
-  # compute sum of each event
-  x_event <- plyr::ddply(x, .(EVENT_ID), plyr::summarise, SUM=sum(VALUE))
+  # compute total sum of each event
+  x_total <- dplyr::group_by(x, EventID)
+  x_total <- dplyr::summarise(x_total, Total=sum(Value))
 
   # take subset of events that meet minimum threshold
-  x_event <- subset(x_event, SUM >= threshold)
+  x_total <- subset(x_total, Total >= threshold.total)
 
-  # assign events an ID of 0 if they did not meet the threshold
-  x$EVENT_ID[which(!(x$EVENT_ID %in% x_event$EVENT_ID))] <- 0
+  # assign wet events an ID of 0 if they did not meet the threshold
+  x[which(!(x$EventID %in% x_total$EventID)), "IsWet"] <- FALSE
 
-  # reassign EVENT based on current EVENT_ID
-  x$EVENT <- x$EVENT_ID > 0
+  # reassign EventID based on current IsWet
+  x$EventID <- c(as.numeric(x$IsWet[1]), diff(x$IsWet))
+  x$EventID <- abs(x$EventID)
+  x$EventID <- cumsum(x$EventID)
 
-  # reassign EVENT_ID based on new EVENT
-  x$EVENT_ID <- c(as.numeric(x$EVENT[1]),
-                  ifelse(diff(x$EVENT) > 0,
-                         diff(x$EVENT), 0))
-  x$EVENT_ID <- cumsum(x$EVENT_ID) * x$EVENT
-
-  # assign DRY_ID for dry periods
-  x$DRY_ID <- c(as.numeric(!x$EVENT[1]),
-                ifelse(diff(!x$EVENT) > 0,
-                       diff(!x$EVENT), 0))
-  x$DRY_ID <- cumsum(x$DRY_ID) * (!x$EVENT)
-
-  # get rle of EVENT_ID
-  x_rle_event <- rle(x$EVENT_ID)
+  # get rle of EventID
+  x_rle_event <- rle(x$EventID)
 
   # add counter for each dry/wet event
-  x$COUNTER <- unlist(sapply(x_rle_event$lengths, seq))
+  x$EventDuration <- unlist(sapply(x_rle_event$lengths, seq))
 
-  # set EVENT_ID during dry events and DRY_ID during wet events to NA
-  x$EVENT_ID[which(x$EVENT_ID == 0)] <- NA
-  x$DRY_ID[which(x$DRY_ID == 0)] <- NA
+  # set WetEventID and DryEventID to NA during opposite event type
+#   x[which(x$WetEventID == 0), "WetEventID"] <- NA
+#   x[which(x$DryEventID == 0), "DryEventID"] <- NA
 
   # assign counter for dry events (number of hours since end of last wet event)
-  x$DRY_HOURS <- ifelse(!is.na(x$DRY_ID), x$COUNTER, 0)
+  # x$DryEventPeriod <- ifelse(!is.na(x$DryEventID), x$Counter, 0)
 
   # assign counter for wet events (number of hours since end of last dry event)
-  x$EVENT_HOURS <- ifelse(!is.na(x$EVENT_ID), x$COUNTER, 0)
+  # x$WetEventPeriod <- ifelse(!is.na(x$WetEventID), x$Counter, 0)
 
-  # add EVENT_ID, DRY_HOURS, WET_HOURS to df and return
-  df$EVENT_ID <- x$EVENT_ID
-  df$EVENT_HOURS <- x$EVENT_HOURS
-  df$DRY_ID <- x$DRY_ID
-  df$DRY_HOURS <- x$DRY_HOURS
+  # add Wet/Dry EventID, EventPeriod to original df and return
+#   df$WetEventID <- x$WetEventID
+#   df$WetEventPeriod <- x$WetEventPeriod
+#   df$DryEventID <- x$DryEventID
+#   df$DryEventPeriod <- x$DryEventPeriod
+  df$EventID <- x$EventID
+  df$EventType <- ifelse(x$IsWet, "Wet", "Dry")
+  df$EventDuration <- x$EventDuration
 
   df
 }
