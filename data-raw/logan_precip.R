25551802bf0e0532eee3bb405c3222d5fe2127ea
Jeff Walker <walker.jeff.d@gmail.com>
2016-02-15 14:52:55 -0500

    upgrade internal logan airport precip dataset through 2015

--- data-raw/logan_precip.R
+++ data-raw/logan_precip.R
@@ -1,6 +1,7 @@
 library(myrwaR)
 
-precip_logan <- load_precip_from_xls(system.file("extdata", "LoganPrecip.xlsx", package = "myrwaR"))
-precip_logan <- subset(precip_logan, !is.na(Precip))
+# precip_logan <- load_precip_from_xls(system.file("extdata", "LoganPrecip.xlsx", package = "myrwaR"))
+precip_logan <- load_precip_from_xls("D:/Dropbox/Work/mystic/_db/Processed/Precip/LoganPrecip.xlsx")
+precip_logan <- precip_logan[!is.na(precip_logan$Precip), ]
 
 devtools::use_data(precip_logan, overwrite = TRUE)
